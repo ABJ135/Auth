677c55f30ca0fc217eab2f577810e57c8cdff571
abj135 <1515.abubakarjaved.m2@gmail.com>
2025-05-01 01:42:11 +0500

    Update user.model.js

--- model/user.model.js
+++ model/user.model.js
@@ -1,34 +1,35 @@
 const { DataTypes } = require('sequelize');
+
 const sequelize = require('../database');
 
 const User = sequelize.define('User', {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    unique: true,
-    allowNull: false,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  isVerified: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: false,
-  },
-  otp: {
-    type: DataTypes.STRING,   
-  },
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    isVerified: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+    },
+    otp: {
+        type: DataTypes.STRING,
+    },
 }, {
-  timestamps: true,  
+    timestamps: true,
 });
 
 module.exports = User;
