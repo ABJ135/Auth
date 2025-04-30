900496945e7e5e8b589927e8a6eb0dc1e71eeb3d
abj135 <1515.abubakarjaved.m2@gmail.com>
2025-04-30 23:45:10 +0500

    Create user.model.js

---
+++ model/user.model.js
@@ -0,0 +1,34 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../database');
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  isVerified: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+  otp: {
+    type: DataTypes.STRING,   
+  },
+}, {
+  timestamps: true,  
+});
+
+module.exports = User;
