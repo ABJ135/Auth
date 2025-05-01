b0f44d9b90239a5cf6251629c312690670bb8d10
abj135 <1515.abubakarjaved.m2@gmail.com>
2025-05-02 02:51:24 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const express = require("express");
 const app = express();
-const port =  3000;
+const port = 3000;
 require("dotenv").config();
 
-const db = require("./model/models"); 
+const db = require("./model/models");
 
-// Routers
 const userRouter = require("./router/User.router");
-
+const catRouter = require("./router/category.router");
+const postRouter = require("./router/post.router");
 
 app.use(express.json());
 
-// Routes
 app.use("/user", userRouter);
+app.use("/category", catRouter);
+app.use("/post", postRouter);
 
-db.sequelize.sync({ alter: true })
-  .then(() => {
-    console.log(" PostgreSQL connected and tables synced");
-    app.listen(port, () => {
-      console.log(`Server is running at http://localhost:${port}`);
-    });
-  })
-  .catch((err) => {
-    console.error(" Database connection error:", err);
-  });
+(async () => {
+  try {
+    await db.sequelize.sync({ alter: true });
+    console.log("Connected to PostgreSQL and synced.");
+    app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
+  } catch (err) {
+    console.error("DB connection failed:", err);
+  }
+})();
